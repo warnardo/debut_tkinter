95544a0b76c43e7a69fa0cfdad766c0b20892ae9
warnardo <Quentin.nion@sfr.fr>
2023-03-31 09:44:38 +0200

    repasse en pack

--- tk_test.py
+++ tk_test.py
@@ -2,17 +2,17 @@
 from tkinter import *
 fenetre = Tk()
 label = Label(fenetre, text="Hello World")
-label.grid(column=0,row=0)
-#label.pack()
+#label.grid(column=0,row=0)
+label.pack()
 label_age= Label(fenetre, text="")
-label_age.grid(column=0,row=1)
-#label_age.pack()
+#label_age.grid(column=0,row=1)
+label_age.pack()
 label_nom=Label(fenetre, text="")
-label_nom.grid(column=0,row=2)
-#label_nom.pack()
+#label_nom.grid(column=0,row=2)
+label_nom.pack()
 bouton_quit=Button(fenetre, text="fermer",command=fenetre.quit)
-bouton_quit.grid(column=0,row=3)
-#bouton_quit.pack()
+#bouton_quit.grid(column=0,row=3)
+bouton_quit.pack()
 
 def age():
     if check_input_num(value.get()):
@@ -29,13 +29,15 @@ def check_input_num(input):
 value = StringVar()
 value.set("texte par défaut")
 entree = Entry(fenetre, textvariable=value, width=30)
-entree.grid(column=0,row=5)
-#entree.pack()
+#entree.grid(column=0,row=5)
+entree.pack()
 
 
 bouton_age=Button(fenetre, text="changer age", command=age, width=15)
-bouton_age.grid(column=0,row=4)
+#bouton_age.grid(column=0,row=4)
+bouton_age.pack()
 bouton_nom=Button(fenetre, text="changer nom", command=nom, width=15)
-bouton_nom.grid(column=1, row=4)
+#bouton_nom.grid(column=1, row=4)
+bouton_nom.pack()
 
 fenetre.mainloop()
