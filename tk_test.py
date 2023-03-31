43451faf5431ebd7e90ffbb90229b0f0c04c3854
warnardo <Quentin.nion@sfr.fr>
2023-03-31 10:51:01 +0200

    ajout frame+retour pack

--- tk_test.py
+++ tk_test.py
@@ -4,12 +4,8 @@ fenetre = Tk()
 label = Label(fenetre, text="Hello World")
 #label.grid(column=0,row=0)
 label.pack()
-label_age= Label(fenetre, text="")
-#label_age.grid(column=0,row=1)
-label_age.pack()
-label_nom=Label(fenetre, text="")
-#label_nom.grid(column=0,row=2)
-label_nom.pack()
+
+
 bouton_quit=Button(fenetre, text="fermer",command=fenetre.quit)
 #bouton_quit.grid(column=0,row=3)
 bouton_quit.pack()
@@ -25,19 +21,33 @@ def check_input_num(input):
     return input.isdigit()
         
 
+
+
+frame1=Frame(fenetre)
+bouton_age=Button(frame1, text="changer age", command=age, width=15)
+#bouton_age.grid(column=0,row=4)
+bouton_age.pack(side=LEFT,padx=10,pady=10)
+label_age= Label(frame1, text="",width=15)
+#label_age.grid(column=0,row=1)
+label_age.pack(side=LEFT,padx=10,pady=10)
+frame1.pack()
+
+
+frame2=Frame(fenetre)
+bouton_nom=Button(frame2, text="changer nom", command=nom, width=15)
+#bouton_nom.grid(column=1, row=4)
+bouton_nom.pack(side=LEFT,padx=10,pady=10)
+label_nom=Label(frame2, text="",width=15)
+#label_nom.grid(column=0,row=2)
+label_nom.pack(side=LEFT,padx=10, pady=10)
+frame2.pack()
+
+
 # entrée
 value = StringVar()
 value.set("texte par défaut")
 entree = Entry(fenetre, textvariable=value, width=30)
 #entree.grid(column=0,row=5)
 entree.pack()
-
-
-bouton_age=Button(fenetre, text="changer age", command=age, width=15)
-#bouton_age.grid(column=0,row=4)
-bouton_age.pack()
-bouton_nom=Button(fenetre, text="changer nom", command=nom, width=15)
-#bouton_nom.grid(column=1, row=4)
-bouton_nom.pack()
 
 fenetre.mainloop()
